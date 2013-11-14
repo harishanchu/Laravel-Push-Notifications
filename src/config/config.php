809e81ce35e0fbd220ec3dfa93adbe3d77979a63
Harish Anchu <harishanchu@gmail.com>
2013-11-14 22:03:46 +0530

    configuration file added

---
+++ src/config/config.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+
+    /*
+	|--------------------------------------------------------------------------
+	| Default Push Connection Name
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify which of the Push connections below you wish
+	| to use as your default connection for all real time push messaging.
+	|
+	*/
+
+    'default' => 'connection1',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Push Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the Push connections setup for your application.
+    |
+    */
+
+    'connections' => array(
+
+        'connection1' => array(
+        ),
+        'connection2' => array(
+        ),
+    ),
+);
