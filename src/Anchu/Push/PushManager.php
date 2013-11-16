90d42126a6ff7bbb49adcebd4d5ea02090e49d58
Harish Anchu <harishanchu@gmail.com>
2013-11-16 22:29:08 +0530

    added methods to manager class

--- src/Anchu/Push/PushManager.php
+++ src/Anchu/Push/PushManager.php
@@ -26,4 +26,39 @@ class PushManager
     {
         $this->app = $app;
     }
+
+    /**
+     * Get the default service name.
+     *
+     * @return string
+     */
+    public function getDefaultService()
+    {
+        return $this->app['config']['push::default'];
+    }
+
+    /**
+     * Get the configuration for a push service.
+     *
+     * @param  string  $name
+     * @return array
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getConfig($name)
+    {
+        $name = $name ?: $this->getDefaultService();
+
+        // To get the push service configuration, we will just pull each of the
+        // service configurations and get the configurations for the given name.
+        // If the configuration doesn't exist, we'll throw an exception and bail.
+        $connections = $this->app['config']['push::services'];
+
+        if (is_null($config = array_get($connections, $name)))
+        {
+            throw new \InvalidArgumentException("Push service [$name] not configured.");
+        }
+
+        return $config;
+    }
 }
