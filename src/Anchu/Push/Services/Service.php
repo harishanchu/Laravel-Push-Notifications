55b293ef5d2e5f317dd24ea6c23b33aa0348e687
Harish Anchu <harishanchu@gmail.com>
2013-12-02 23:00:28 +0530

    curl init utility method added

--- src/Anchu/Push/Services/Service.php
+++ src/Anchu/Push/Services/Service.php
@@ -2,4 +2,30 @@
 
 class Service {
 
+    /**
+     * Utility function used to create the curl object with common settings
+     *
+     * @param $url
+     * @param $timeout
+     * @param string $request_method
+     * @param array $query_params
+     * @return resource
+     * @throws \Exception
+     */
+    private function create_curl($url, $timeout, $request_method = 'GET', $query_params = array())
+    {
+        $ch = curl_init();
+        if ( $ch === false )
+        {
+            throw new \Exception('Could not initialise cURL!');
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array ( "Content-Type: application/json" ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+
+        return $ch;
+    }
+
 }
